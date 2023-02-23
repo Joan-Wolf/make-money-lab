48b607d1eb4a82274387115f5afaf935ed7a50ea
Joan Wolf <joanewolf@gmail.com>
2023-02-22 19:06:39 -0500

    seecond commit

--- makemoney.js
+++ makemoney.js
@@ -5,6 +5,8 @@
     const coinContainer = document.getElementById('coin-container');
     function makeMoney(event) {
         event.preventDefault();
+        //the above is literally only because the HTML is in a form tag, it's trying
+        //to submit to a backend
         const howManyInputValue = +howManyInput.value;
         for (let i = 0; i<howManyInputValue; i++) {
         const newCoin = document.createElement('div');
@@ -12,17 +14,20 @@
         newCoinP.innerText = coinType.value;
         newCoin.append(newCoinP);
         const coinTypeValue = coinType.value
-        if (coinTypeValue === "penny") {
-            newCoin.classList.add("penny");
-        } else if (coinTypeValue === "nickel") {
-            newCoin.classList.add("nickel");
-        } else if (coinTypeValue === "quarter") {
-            newCoin.classList.add("quarter")
-        }
-         else {
-        newCoin.classList.add('coin');
+        newCoin.classList.add(coinTypeValue.value);
+        //the above only works because what we named our value and classes is the same
+        //it replaces the else if statements
+        // if (coinTypeValue === "penny") {
+        //     newCoin.classList.add("penny");
+        // } else if (coinTypeValue === "nickel") {
+        //     newCoin.classList.add("nickel");
+        // } else if (coinTypeValue === "quarter") {
+        //     newCoin.classList.add("quarter")
+        // }
+        //  else {
+        // newCoin.classList.add('coin');
         
-        }
+        // }
         newCoin.addEventListener('click', (event) => {
             if (event.target.classList.contains("coin")){
                 event.target.remove();
